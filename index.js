dd4ccdf508f80e555feb8cdce86204a1bbf20122
vimrus <vimrus.com@gmail.com>
2017-08-14 00:28:13 +0800

    * fix.

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-$.get('http://daily.zhihu.com', function(data, status, xhr) {
-		$(data).find('.box a').each(function (index, value) {
-			var url  = 'http://daily.zhihu.com' + $(this).attr('href');
+$.getJSON('https://news-at.zhihu.com/api/4/news/latest', function(error, response, data) {
+		data.stories.forEach(function (story) {
+			var url = 'https://news-at.zhihu.com/api/4/news/' + story.id;
 			var title = $(this).text();
 			articles.get('url', url, function(article) {
 				if (article) return;
 
-				$.get(url, function(data, status, xhr) {
+				$.getJSON(url, function(error, response, data) {
 					var html = $(data).find('.content-inner').eq(0);
 
 					var article = {
 						title: title,
-						content: html.html().trim(),
-						summary: html.text().trim().substring(0, 50),
-						url: url
+						content: data.body,
+						summary: data.body.replace(/<\/?[^>]*>/g,'').trim().substring(0, 50),
+						url: url,
+						image: data.images[0]
 					};
 					articles.append(article);
 				});
