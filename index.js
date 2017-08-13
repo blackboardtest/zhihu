246246d186566328de56c78fe4300d765f675349
vimrus <vimrus.com@gmail.com>
2017-08-14 00:32:16 +0800

    * fix.

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 $.getJSON('https://news-at.zhihu.com/api/4/news/latest', function(error, response, data) {
 		data.stories.forEach(function (story) {
 			var url = 'https://news-at.zhihu.com/api/4/news/' + story.id;
-			var title = $(this).text();
 			articles.get('url', url, function(article) {
 				if (article) return;
 
 				$.getJSON(url, function(error, response, data) {
 					var article = {
-						title: title,
+						title: data.title,
 						content: data.body,
 						summary: data.body.replace(/<\/?[^>]*>/g,'').trim().substring(0, 50),
 						url: url,
