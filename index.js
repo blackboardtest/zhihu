e6c32fd36bba7a6958f7870a330c9d8fd8987617
vimrus <vimrus.com@gmail.com>
2017-08-29 15:53:38 +0800

    * fix.

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 hubble.getJSON('https://news-at.zhihu.com/api/4/news/latest', function(error, response, data) {
 		data.stories.forEach(function (story) {
-			var url = 'https://news-at.zhihu.com/api/4/news/' + story.id;
-			articles.get('url', url, function(article) {
+			articles.get('id', story.id, function(article) {
 				if (article) return;
 
+				var url = 'https://news-at.zhihu.com/api/4/news/' + story.id;
 				hubble.getJSON(url, function(error, response, data) {
 					var article = {
+						id: data.id,
 						title: data.title,
 						content: data.body,
 						summary: data.body.replace(/<\/?[^>]*>/g,'').trim().substring(0, 50),
-						url: url,
+						url: data.share_url,
 						image: data.images[0]
 					};
 					articles.append(article);
